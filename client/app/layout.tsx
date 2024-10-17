bfadc24f97ed64d2a01a3fd0372c6d6705c054c0
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-10-17 22:18:08 +0300

    Use montserrat font as global font

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,17 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Montserrat } from "next/font/google"
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const montserrat = Montserrat({
+  subsets: ["cyrillic", "latin"]
+})
 
 export const metadata: Metadata = {
   title: "Notefill",
@@ -25,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${montserrat.className} antialiased`}
       >
         {children}
       </body>
